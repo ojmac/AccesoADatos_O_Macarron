bc28f7105c17d5b3a754f8450ab6d1e54042f45b
Ojmacmac <ojmacmac@gmail.com>
2024-10-16 20:18:57 +0200

    nueva actualizacion 16/10

---
+++ File/src/ejercicios/Ej_5_1_1Test.java
@@ -0,0 +1,16 @@
+package ejercicios;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Ej_5_1_1Test {
+
+	Ej_5_1_1 ej =new Ej_5_1_1();
+	
+	@Test
+	void leerXMLTest() {
+		ej.leerXML();
+	}
+	
+}
