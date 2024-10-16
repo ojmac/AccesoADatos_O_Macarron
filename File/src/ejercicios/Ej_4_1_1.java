43e17665f7757b417eb8c3bdb5cebbd70bcd2020
Ojmacmac <ojmacmac@gmail.com>
2024-10-16 18:19:15 +0200

    Actualizacion 16-10

--- File/src/ejercicios/Ej_4_1_1.java
+++ File/src/ejercicios/Ej_4_1_1.java
@@ -1,8 +1,12 @@
 package ejercicios;
 
 import java.io.DataInputStream;
+import java.io.EOFException;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 
 public class Ej_4_1_1 {
 
@@ -11,27 +15,61 @@ public class Ej_4_1_1 {
 	 *El método tendrá como parámetro la matrícula del coche a buscar.
 	 *Emplea un método de Test para la ejecución del nuevo método.
 	 */
-	public void datosCoches2(String matricula) {
+	
+	public void guardaCoches2(String[] marcas, String[] matriculas, int[] anyosMatriculacion) {
 
-		
+		String nomArch = "./coches.obj";
 		try {
-			String nomArch = "./" + matricula + ".dat"; 
-			FileInputStream fis = new FileInputStream(nomArch);
-			DataInputStream dis = new DataInputStream(fis);
-			
-			String marca = dis.readUTF();
-	        String matriculaL = dis.readUTF();
-	        int anyoMatriculacion = dis.readInt();
-			dis.close();
-	        
-			System.out.printf("El coche del año : %d, marca: %s, con matricula %s \n", anyoMatriculacion, marca,matriculaL);
-			
+			FileOutputStream fos = new FileOutputStream(nomArch, true);
+			ObjectOutputStream oos = new ObjectOutputStream(fos);
+
+			for (int i = 0; i < marcas.length; i++) {
+				Coche c = new Coche(marcas[i], matriculas[i], anyosMatriculacion[i]);
+				oos.writeObject(c);
+				System.out.println("Coche guardado en " + nomArch);
+			}
+
+			oos.close();
 
 		} catch (IOException e) {
-			// TODO Bloque catch generado automáticamente
-			e.printStackTrace();
+
 		}
-           
-	}																										
+	}
 	
+	public void datosCoches2(String nomFich, String matricula) {
+		ObjectInputStream ois=null; 
+		try {
+			FileInputStream fis = new FileInputStream(nomFich);
+			ois = new ObjectInputStream(fis);
+			while (true) {
+
+				try {
+					Coche pOut = (Coche) ois.readObject();
+					
+					if(matricula.equals(pOut.matricula)) {
+					System.out.printf("El coche del año : %d, marca: %s, con matricula %s \n", pOut.anyoMatriculacion,
+							pOut.marca, pOut.matricula);
+					break;
+					}else continue;
+				}catch (EOFException e) {
+	               
+	                break; 
+	            }catch (ClassNotFoundException e) {
+
+					e.printStackTrace();
+				}
+			}
+			
+		} catch (IOException e) {
+
+			e.printStackTrace();
+		}finally{
+			try {
+				ois.close();
+			} catch (IOException e) {
+				// TODO Bloque catch generado automáticamente
+				e.printStackTrace();
+			}
+		}
+	}
 }
