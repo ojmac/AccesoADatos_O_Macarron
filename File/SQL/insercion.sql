e79f7d417865b5dcf1abc8c91d1c6b69d6739233
Ojmacmac <ojmacmac@gmail.com>
2024-11-06 11:46:44 +0100

    act 6-11

--- File/SQL/insercion.sql
+++ File/SQL/insercion.sql
@@ -1,10 +1,10 @@
-INSERT INTO tareas_pendientes (id, descripcion, fecha_inicio, fecha_final, finalizada) VALUES
-(1, 'Comprar manzanas', '2024-02-15', '2024-02-16', TRUE),
-(2, 'Estudiar examen', '2024-05-02', '2024-05-18', TRUE),
-(3, 'Comprar billete para Sidney', '2024-02-15', '2024-05-20', TRUE),
-(4, 'Ir a Australia', '2024-06-01', '2024-08-25', TRUE),
-(5, 'Revisión del coche', '2024-10-08', '2024-10-16', TRUE),
-(6, 'Otra cosa', '2024-10-01', '2024-12-01', FALSE),
-(7, 'Práctica JDBC', '2024-10-30', '2024-11-07', FALSE),
-(8, 'FCT', '2025-03-01', '2025-05-24', FALSE);
+INSERT INTO tareas_pendientes  VALUES (1, 'Comprar manzanas', '2024-02-15', '2024-02-16', TRUE);
+INSERT INTO tareas_pendientes  VALUES(1, 'Comprar manzanas', '2024-02-15', '2024-02-16', TRUE);
+INSERT INTO tareas_pendientes  VALUES(2, 'Estudiar examen', '2024-05-02', '2024-05-18', TRUE);
+INSERT INTO tareas_pendientes  VALUES(3, 'Comprar billete para Sidney', '2024-02-15', '2024-05-20', TRUE);
+INSERT INTO tareas_pendientes  VALUES(4, 'Ir a Australia', '2024-06-01', '2024-08-25', TRUE),
+INSERT INTO tareas_pendientes  VALUES(5, 'Revisión del coche', '2024-10-08', '2024-10-16', TRUE);
+INSERT INTO tareas_pendientes  VALUES(6, 'Otra cosa', '2024-10-01', '2024-12-01', FALSE);
+INSERT INTO tareas_pendientes  VALUES(7, 'Práctica JDBC', '2024-10-30', '2024-11-07', FALSE);
+INSERT INTO tareas_pendientes  VALUES(8, 'FCT', '2025-03-01', '2025-05-24', FALSE);
 
